a3402987810e1ff0c9b8112b5aa2b9257ba00e04
IgorOfKaleidoscopic <igor.votava@kaleidoscopic.me>
2023-02-02 13:11:19 +0100

    Using Dayjs

--- src/app/core/agenda-data/agenda-data.service.ts
+++ src/app/core/agenda-data/agenda-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 import { IProject, Project } from 'src/app/shared/models/project';
 import { ITask, Task } from 'src/app/shared/models/task';
@@ -14,10 +15,10 @@ export class AgendaDataService {
     this.projects.push(new Project(1, "Umenie"));
     this.projects.push(new Project(2, "Software"));
 
-    this.tasks.push(new Task(1, 1, "Hudobna teoria", "Vyskusaj precitane z hudobnej teorie", new Date('2024-02-01')));
-    this.tasks.push(new Task(2, 1, "Kresba", "Urob si kurz ilustracie", new Date('2024-03-01')));
-    this.tasks.push(new Task(1, 2, "Obsidian", "Migruj data do Obsidianu", new Date('2024-01-05')));
-    this.tasks.push(new Task(2, 2, "UX", "Citaj knihu o UX", new Date('2024-01-11')));
+    this.tasks.push(new Task(1, 1, 'Hudobna teoria', 'Vyskusaj precitane z hudobnej teorie', dayjs('2024-03-11', 'YYYY-MM-DD')));
+    this.tasks.push(new Task(2, 1, 'Kresba', 'Urob si kurz ilustracie', dayjs('2024-03-01', 'YYYY-MM-DD')));
+    this.tasks.push(new Task(1, 2, 'Obsidian', 'Migruj data do Obsidianu', dayjs('2024-05-05', 'YYYY-MM-DD')));
+    this.tasks.push(new Task(2, 2, 'UX', 'Citaj knihu o UX', dayjs('2024-01-11', 'YYYY-MM-DD')));
 
     console.log("AgendaDataService:constructor() 999");
   }
--- src/app/shared/models/task.ts
+++ src/app/shared/models/task.ts
@@ -1,9 +1,11 @@
+import { Dayjs } from 'dayjs';
+
 export interface ITask {
   id:number;
   idp: number;
   subject: string;
   detail: string;
-  deadline: Date;
+  deadline: Dayjs;
 }
 
 export class Task implements ITask {
@@ -11,9 +13,9 @@ export class Task implements ITask {
   public idp: number
   public subject: string;
   public detail: string;
-  public deadline: Date;
+  public deadline: Dayjs;
 
-  constructor(id:number, idp:number, subject: string, detail:string, deadline: Date) {
+  constructor(id:number, idp:number, subject: string, detail:string, deadline: Dayjs) {
     this.id = id;
     this.idp = idp;
     this.subject = subject;
